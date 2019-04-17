b7a7f18fa8d5dbfd04bbc5d6a2098d4b5b24a92d
George Rushton <grushton94@yahoo.com>
2019-04-17 11:47:28 +0100

    added pv_def back to edit_run

--- ips/static/js/edit-run.js
+++ ips/static/js/edit-run.js
@@ -57,13 +57,13 @@ $(document).ready(function(e){
                     }
                     builds[block][expression] = {const: consta, var: va, opp: opp, val: val, command: command};
                 });
-
+                pv_desc = $(this).children(".pv_def").text();
                 myJsonString = JSON.stringify(builds);
                 $.ajax({
                     type: "POST",
                     url: 'http://127.0.0.1:5000/builder/' + $("#rid").text() + '/' + pvid,
                     contentType: 'application/x-www-form-urlencoded',
-                    data: {json: myJsonString},
+                    data: {json: myJsonString, pv: pv_desc},
                     async:false
                 });
             //}
