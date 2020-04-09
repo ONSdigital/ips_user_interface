45e65bf26687c2eb16a85c11aa5d6ea3d271a306
Andy Urquhart <35800749+urquha@users.noreply.github.com>
2020-04-09 11:48:21 +0100

    bug fixed

--- ips/services/new_run_steps/step_4.py
+++ ips/services/new_run_steps/step_4.py
@@ -7,11 +7,15 @@ def run_step_4(run_id, template_id):
     if request.method == 'POST':
         return redirect('/new_run_steps/new_run_5/' + run_id)
 
+    if template_id is None:
+        if session['template_id']:
+            template_id = session['template_id']
+
     run_id = session['id']
 
     header = ['PV_NAME', 'PV_REASON', 'PV_CONTENT']
 
-    records = app_methods.get_process_variables(run_id)
+    records = app_methods.get_process_variables(template_id)
 
     return render_template('new_run_4.html', run_id=run_id, table=records,
                            header=header, api_target=API_TARGET,)
